dccbab1f08c1f7c32429c397910c739270ce8911
chip_luxury <chip_luxury@gmail.com>
2021-06-22 12:48:26 +0300

    154

--- U1/chatlib.py
+++ U1/chatlib.py
@@ -1,74 +1,64 @@
 #!/bin/python3
-#changes made by chip_luxury.
+# changes made by chip_luxury.
 
 # Protocol Constants
-CMD_FIELD_LENGTH = 16	# Exact length of cmd field (in bytes)
+CMD_FIELD_LENGTH = 16  # Exact length of cmd field (in bytes)
 LENGTH_FIELD_LENGTH = 4   # Exact length of length field (in bytes)
-MAX_DATA_LENGTH = 10**LENGTH_FIELD_LENGTH-1  # Max size of data field according to protocol
+# Max size of data field according to protocol
+MAX_DATA_LENGTH = 10**LENGTH_FIELD_LENGTH-1
 MSG_HEADER_LENGTH = CMD_FIELD_LENGTH + 1 + LENGTH_FIELD_LENGTH + 1  # Exact size of header (CMD+LENGTH fields)
 MAX_MSG_LENGTH = MSG_HEADER_LENGTH + MAX_DATA_LENGTH  # Max size of total message
 DELIMITER = "|"  # Delimiter character in protocol
 DATA_DELIMITER = "#"  # Delimiter in the data part of the message
+ERROR_RETURN = None  # What is returned in case of an error
 
-# Protocol Messages 
+# Protocol Messages
 # In this dictionary we will have all the client and server command names
 
 PROTOCOL_CLIENT = {
-"login_msg" : "LOGIN",
-"logout_msg" : "LOGOUT"
-} # .. Add more commands if needed
-
+    "login_msg": "LOGIN",
+    "logout_msg": "LOGOUT",
+    "log_state": "LOGGED",
+    "get_quetion": "GET_QUESTION",
+    "send_ans": "SEND_ANSWER",
+    "my_score": "MY_SCORE",
+    "highscore": "HIGHSCORE"
+}  # .. Add more commands if needed
 
 PROTOCOL_SERVER = {
-"login_ok_msg" : "LOGIN_OK",
-"login_failed_msg" : "ERROR"
-} # ..  Add more commands if needed
+    "login_ok_msg": "LOGIN_OK",
+    "login_failed_msg": "ERROR",
+    "logged_ans": "LOGGED_ANSWER",
+    "your_question": "YOUR_QUESTION",
+    "correct_ans": "CORRECT_ANSWER",
+    "wrong_ans": "WRONG_ANSWER",
+    "your_score": "YOUR_SCORE",
+    "all_score": "ALL_SCORE",
+    "no_question": "NO_QUESTIONS"
+}  # ..  Add more commands if needed
 
+def build_message(cmd, data):  # creating a msg from cmd string and data string
+    if cmd in PROTOCOL_SERVER.values() or cmd in PROTOCOL_CLIENT.values():
+        return cmd.ljust(CMD_FIELD_LENGTH) + DELIMITER + str(len(data)).zfill(4) + DELIMITER + data
+    else:
+        return ERROR_RETURN
 
-# Other constants
+def parse_message(data):  # extract cmd and data from a message
+    cmd = data.split(DELIMITER, 3)[0].strip()
+    msg_len = data.split(DELIMITER, 3)[1].strip()
+    msg = data.split(DELIMITER, 3)[2].strip()
+    if (data.count("|") == 2 and
+        msg_len.isnumeric() and
+            int(msg_len) == len(msg)):
+        return cmd, msg
+    else:
+        return ERROR_RETURN
 
-ERROR_RETURN = None  # What is returned in case of an error
+def split_data(msg, expected_fields):  # split the parameters from the data string
+    if msg.count("#") == expected_fields - 1:
+        return msg.split(DATA_DELIMITER, expected_fields - 1)
+    else:
+        return ERROR_RETURN
 
-
-def build_message(cmd, data):
-	"""
-	Gets command name (str) and data field (str) and creates a valid protocol message
-	Returns: str, or None if error occured
-	"""
-	full_msg = 1
-	return full_msg
-
-
-def parse_message(data):
-	"""
-	Parses protocol message and returns command name and data field
-	Returns: cmd (str), data (str). If some error occured, returns None, None
-	"""
-    # Implement code ...
-
-    # The function should return 2 values
-    # return cmd, msg
-
-	
-def split_data(msg, expected_fields):
-	"""
-	Helper method. gets a string and number of expected fields in it. Splits the string 
-	using protocol's data field delimiter (|#) and validates that there are correct number of fields.
-	Returns: list of fields if all ok. If some error occured, returns None
-	"""
-	# Implement code ...
-	if msg.count("#") == expected_fields - 1:
-		fields = msg.split(DATA_DELIMITER, expected_fields - 1)
-		return fields
-	else:
-		return None
-	#todo: add try/except
-
-
-def join_data(msg_fields):
-	"""
-	Helper method. Gets a list, joins all of it's fields to one string divided by the data delimiter. 
-	Returns: string that looks like cell1#cell2#cell3
-	"""
-	# Implement code ...
-	return DATA_DELIMITER.join(str(i) for i in msg_fields)	
+def join_data(msg_fields):  # join parameters from list to data message
+    return DATA_DELIMITER.join(str(i) for i in msg_fields)
--- U1/my_tests.py
+++ U1/my_tests.py
@@ -1,17 +1,21 @@
 #!/bin/python3
 
-from chatlib import split_data
-from chatlib import join_data
-
+from chatlib import *
 
 msg7 = "username#password"
 msg8 = "test#username#pa#ssword#foo"
 
-a = split_data(msg7 ,3)
+a = split_data(msg8 ,5)
 
-# print(a)
+print(a)
 
 #b = join_data(["username" , "password"])
 b = join_data(["question" , "ans1" , "ans2" , "ans3" , "ans4" , "correct", 34, 35.52, "fff", 23])
 
-print(b)
+# print(b)
+
+c = build_message("LOGIN", "user#pass")
+# print(c)
+
+d = parse_message("LOGIN           |  Z9|user#pass")
+print((d))
