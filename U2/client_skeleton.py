853c57ee1ec5ee5081565523dc6cf8653cb79914
chip_luxury <chip_luxury@gmail.com>
2021-07-04 14:48:18 +0300

    minor fix

--- U2/client_skeleton.py
+++ U2/client_skeleton.py
@@ -7,64 +7,42 @@ import chatlib  # To use chatlib functions or consts, use chatlib.****
 SERVER_IP = "127.0.0.1"  # Our server will run on same computer as client
 SERVER_PORT = 5678
 
-# HELPER SOCKET METHODS
-
 def build_and_send_message(conn, code, data):
-	"""
-	Builds a new message using chatlib, wanted code and message. 
-	Prints debug info, then sends it to the given socket.
-	Paramaters: conn (socket object), code (str), data (str)
-	Returns: Nothing
-	"""
-	# Implement Code
-	
+	msg = chatlib.build_message(code, data)
+	print("debug build message: ", msg)
+	conn.send(msg.encode())
 
 def recv_message_and_parse(conn):
-	"""
-	Recieves a new message from given socket,
-	then parses the message using chatlib.
-	Paramaters: conn (socket object)
-	Returns: cmd (str) and data (str) of the received message. 
-	If error occured, will return None, None
-	"""
-	# Implement Code
-	# ..
-	
+	full_msg = conn.recv(1024).decode()
 	cmd, data = chatlib.parse_message(full_msg)
+	# cmd, data = chatlib.parse_message("LOGIN           |   9|user#pass") #testing
 	return cmd, data
-	
-	
 
 def connect():
-    # Implement Code
+	# Implement Code
 	trivia_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	trivia_socket.connect((SERVER_IP, SERVER_PORT))
-	
 	return trivia_socket
 
-
 def error_and_exit(error_msg):
-    # Implement code
-    pass
-
+	print(error_msg)
+	print("exiting....")
+	exit
 
 def login(conn):
-    username = input("Please enter username: \n")
-    # Implement code
-	
-	# build_and_send_message(conn, chatlib.PROTOCOL_CLIENT["login_msg"], "")
-	
+	username = input("Please enter username: ")
+	password = input("please enter password: ")
+	data = chatlib.join_data([username, password])
 	# Implement code
-	
-    pass
+	build_and_send_message(conn, chatlib.PROTOCOL_CLIENT["login_msg"], data)
 
 def logout(conn):
-    # Implement code
-    pass
+	build_and_send_message(conn, chatlib.PROTOCOL_CLIENT["logout_msg"], "")
 
 def main():
-    # Implement code
-    pass
-
+	my_socket = connect()
+	login(my_socket)
+	# logout(my_socket)
+	# my_socket.close()
 if __name__ == '__main__':
-    main()
+	main()
