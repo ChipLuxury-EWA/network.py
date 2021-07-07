b3284278d6490f71d3c34d40af4e0d58c8fbdbb0
chip_luxury <chip_luxury@gmail.com>
2021-07-07 09:38:18 +0300

    following udp: client&server

---
+++ U3/client_udp_followalong.py
@@ -0,0 +1,14 @@
+#!/bin/python3
+import socket
+
+SERVER_IP = "127.0.0.1"
+SERVER_PORT = 8821
+MMS = 1024
+
+my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+send_msg = input("write your message: ")
+
+my_socket.sendto(send_msg.encode(), (SERVER_IP, SERVER_PORT))
+(msg, remote_address) = my_socket.recvfrom(MMS)
+print("[SERVER] - response: ", msg.decode())
+my_socket.close()
