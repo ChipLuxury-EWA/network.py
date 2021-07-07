02b45f612e950452d4be534a510ea46ee512dce5
chip_luxury <chip_luxury@gmail.com>
2021-07-07 10:22:06 +0300

    working udp with while loop, first tray!!!

--- U3/client_udp_followalong.py
+++ U3/client_udp_followalong.py
@@ -6,9 +6,14 @@ SERVER_PORT = 8821
 MMS = 1024
 
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-send_msg = input("write your message: ")
 
-my_socket.sendto(send_msg.encode(), (SERVER_IP, SERVER_PORT))
-(msg, remote_address) = my_socket.recvfrom(MMS)
-print("[SERVER] - response: ", msg.decode())
+while True:
+    send_msg = input("write your message: ")
+    my_socket.sendto(send_msg.encode(), (SERVER_IP, SERVER_PORT))
+    (msg, remote_address) = my_socket.recvfrom(MMS)
+    print("[SERVER] - response: ", msg.decode())
+    
+    if send_msg == "EXIT":
+        break
+
 my_socket.close()
--- U3/server_udp_followalong.py
+++ U3/server_udp_followalong.py
@@ -6,9 +6,13 @@ SERVER_PORT = 8821
 MMS = 1024
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_socket.bind((SERVER_IP, SERVER_PORT))
-(client_msg, client_address) = server_socket.recvfrom(MMS)
-msg = client_msg.decode()
-print("client: ", client_address, "sent this message: ", msg)
-server_response = "got your message!: " + msg
-server_socket.sendto(server_response.encode(), client_address)
+
+while True:
+    (client_msg, client_address) = server_socket.recvfrom(MMS)
+    msg = client_msg.decode()
+    print("client: ", client_address, "sent this message: ", msg)
+    server_response = "got your message!: " + msg
+    server_socket.sendto(server_response.encode(), client_address)
+    if msg == "EXIT":
+        break
 server_socket.close()
